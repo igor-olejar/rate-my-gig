920150e7932f26bedc7ef35aaa493e3be3469a7e
Igor Olejar <igor.olejar@gmail.com>
2023-12-23 17:04:25 +0000

    User and ratings relationship defined

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Rating extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'rating',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
