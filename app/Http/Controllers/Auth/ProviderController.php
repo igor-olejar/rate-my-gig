bbe6526b2b5cc90e3c04fcc88345083fda1e80f3
Igor Olejar <igor.olejar@gmail.com>
2023-12-24 15:31:36 +0000

    Making user password nullable

--- app/Http/Controllers/Auth/ProviderController.php
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Laravel\Socialite\Facades\Socialite;
 
 class ProviderController extends Controller
@@ -14,7 +15,15 @@ class ProviderController extends Controller
 
     public function callback(string $provider)
     {
-        $user = Socialite::driver($provider)->user();
-        dd($user);
+        $socialUser = Socialite::driver($provider)->user();
+
+        $user = User::updateOrCreate([
+            'id' => $socialUser->id,
+        ], [
+            'name'=> $socialUser->name,
+            'email'=> $socialUser->email,
+            'claimed' => true,
+            'remember_token' => $socialUser->token,
+        ]);
     }
 }
